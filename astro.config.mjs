c6c6385029e051af88acef2e471ad74a1ad2f97e
Paul Jeter <paul.jeter@resi.io>
2025-10-28 14:50:31 -0500

    add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), robotsTxt()],
 });
